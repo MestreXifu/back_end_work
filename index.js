712f8c03a9fcce55328cb1f480ac279c3815a423
Xifu <joaopedrosartoritrabalho@gmail.com>
2024-10-08 22:03:24 -0300

    busca_cliente

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ let clientes = [
     { id: 2, name: 'João Pedro', email: 'joao.pedro@gmail.com' },
     { id: 3, name: 'Douglas', email: 'douglas@gmail.com' }
 ];
+
+// Busca de cliente por ID
+app.get('/customers/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const cliente = clientes.find(c => c.id === id);
+
+    if (!cliente) {
+        return res.status(404).json({ message: 'Cliente não encontrado' });
+    }
+
+    res.status(200).json(cliente);
+});
